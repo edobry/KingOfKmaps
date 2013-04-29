771216a7756032260f2d3d37f357dd4a1dd63bce
edobry <edobry@gmail.com>
2013-04-29 02:21:14 -0400

    Starting over with interface-first dev

---
+++ KingOfKmaps/Models/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+
+namespace KingOfKmaps.Models
+{
+    public class Game
+    {
+        public ObjectId PlayerOne { get; set; }
+        public ObjectId PlayerTwo { get; set; }
+
+        public IEnumerable<Move> Moves { get; set; }
+    }
+
+    public class Move
+    {
+        public int Player { get; set; }
+        public Tuple<int, int> Position { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
---
+++ KingOfKmaps/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace KingOfKmaps.Models
+{
+    public class User
+    {
+        public ObjectId id;
+        public string username;
+        public string First;
+        public string Last;
+
+        [BsonIgnore]
+        public string FullName
+        {
+            get
+            {
+                var name = First + " " + Last;
+                return !String.IsNullOrEmpty(name) ? name : username;
+            }
+        }
+    }
+}
